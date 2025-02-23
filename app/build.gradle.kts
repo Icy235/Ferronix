12b5719be7a4b8cfd65fc16b56ba6c4345c0108d
Icy235 <daniil.ivanov2077@mail.ru>
2025-02-23 18:36:13 +0300

    Ferronix V0.4.1 Исправлены визуальные ошибки(белая тема не работала адекватно) Закинул заголовки в стили Вопрос с фоном, не могу выбрать

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "com.calculate.ferronix"
         minSdk = 24
         targetSdk = 35
-        versionCode = 3
-        versionName = "0.4.0"
+        versionCode = 4
+        versionName = "0.4.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         versionNameSuffix = "alfa"
